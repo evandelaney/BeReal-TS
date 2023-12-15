17ebb622aeef8fb770407517d83d4eabd3792251
Evan DeLaney <yo@evan.pizza>
2023-12-14 20:15:46 -0500

    Initial Commit

---
+++ BeReal-TS/BeReal_TSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeReal_TSApp.swift
+//  BeReal-TS
+//
+//  Created by Evan DeLaney on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct BeReal_TSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeReal-TS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeReal-TS
+//
+//  Created by Evan DeLaney on 12/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
